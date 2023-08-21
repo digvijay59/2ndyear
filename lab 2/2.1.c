08fe3bf66ccc08b8dde1e588aaea08b3b11dfbaa
digvijay59 <83942542+digvijay59@users.noreply.github.com>
2023-08-21 07:29:47 +0000

    done

---
+++ lab 2/2.1.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+int main()
+{
+    int insert(){
+        return 0;
+    }
+
+    int del(){
+        return 0;
+    }
+
+    int lsearch(){
+        return 0;
+    }
+
+    int traverse(){
+        return 0;
+    }
+
+    int exit(){
+        return 0;
+    }
+    int n;
+    printf("enter the length of array");
+    scanf("%d",&n);
+    int a[n];
+    int i;
+    printf("enter the elements");
+    for (i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int *ptr=&a[0];
+    while(1)
+    {
+        int choice;
+        printf("Press 1 to insert.\n");
+        printf("Press 2 to delete\n");
+        printf("Press 3 to lenear search\n");
+        printf("Press 4 to traverse\n");
+        printf("Press 5 to exit\n");
+        scanf("%d", &choice);
+        if (choice==1){
+            insert(ptr,n);
+        }
+        else if (choice==2)
+        {
+            del(ptr,n);
+        }
+        else if (choice==3)
+        {
+            lsearch(ptr,n);
+        }
+        else if (choice==4)
+        {
+            traverse(ptr,n);
+        }
+        else if (choice==5)
+        {
+            exit();
+        }
+    }
+    
+}
